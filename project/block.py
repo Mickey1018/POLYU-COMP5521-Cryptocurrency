d51e2d6da4ff9a8b7d103473baf815e84024ccff
Mickey1018 <58300665+Mickey1018@users.noreply.github.com>
2020-11-03 18:01:16 +0800

    Update block.py

--- project/block.py
+++ project/block.py
@@ -28,25 +28,3 @@ class Block:
         block_params = {x: self.__dict__[x] for x in self.__dict__ if x not in ignore}
         block_string = json.dumps(str(block_params), sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
-
-    def __jsonencode__(self):
-        return {'block': self.__dict__}
-
-
-class AdvancedJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if hasattr(obj, '__jsonencode__'):
-            return obj.__jsonencode__()
-        if isinstance(obj, set):
-            return list(obj)
-        return json.JSONEncoder.default(self, obj)
-
-
-# test
-# test_block = Block(100, '0', time.time(), {}, 4, 0)
-# print(test_block.compute_hash())
-# print(test_block.hash)
-# print(test_block.compute_hash())
-# print(test_block.__dict__)
-# print(test_block.toJSON())
-# print(type(test_block.toJSON()))
